d585054b60537da6148f6e2ebf8c44b8c8dae290
thangcao9thwonder <cvthang56th2@gmail.com>
2021-08-23 15:31:15 +0700

    update test

--- cypress/integration/example_new.js
+++ cypress/integration/example_new.js
@@ -57,36 +57,45 @@ describe('Testing 9thWonder Website 1', () => {
     // case 2: Reload page, check result = result case 1
     cy.reload()
     // reload không cần đợi fetch api
-    cy.window().then(win => {
-      const containers=  win.$('.result-containter')
-      const container = containers[0]
-      const $els = container.querySelectorAll('.col-item-result h5')
+    cy.window()
+      .then(win => {
+        const containers=  win.$('.result-containter')
+        const container = containers[0]
+        const $els = container.querySelectorAll('.col-item-result h5')
 
-      cy.get('@countLastResult').then(count => {
-        expect($els.length).to.equal(count)
-      })
+        const firstItemTitle = $els[0].innerText
+        const thirdItemTitle = $els[2].innerText
+        const lastItemTitle = $els[$els.length - 1].innerText
+        const countLastResult = $els.length
 
-      const firstItemTitle = $els[0].innerText
-      const thirdItemTitle = $els[2].innerText
-      const lastItemTitle = $els[$els.length - 1].innerText
-      
-      // check item đầu có bắt đầu bằng Honda
-      const splitArr1 = firstItemTitle.split(' ')
-      expect(splitArr1[0]).to.equal('Honda')
-      
-      // check item cuối có kết thúc bằng Guide
-      const splitArr3 = thirdItemTitle.split(' ')
-      expect(splitArr3[splitArr3.length - 1]).to.equal('Guide')
+        return {
+          firstItemTitle,
+          thirdItemTitle,
+          lastItemTitle,
+          countLastResult
+        }
+      })
+      .then(({ firstItemTitle, thirdItemTitle, lastItemTitle, countLastResult }) => {
+        cy.get('@countLastResult').then(count => {
+          expect(countLastResult).to.equal(count)
+        })
 
-      // check kết quả đầu và cuối vẫn match với case1
-      cy.get('@firstItemTitle').then(case1FirstItemTitle => {
-        expect(case1FirstItemTitle).to.equal(firstItemTitle)
+        // check item đầu có bắt đầu bằng Honda
+        const splitArr1 = firstItemTitle.split(' ')
+        expect(splitArr1[0]).to.equal('Honda')
+        
+        // check item cuối có kết thúc bằng Guide
+        const splitArr3 = thirdItemTitle.split(' ')
+        expect(splitArr3[splitArr3.length - 1]).to.equal('Guide')
+
+        // check kết quả đầu và cuối vẫn match với case1
+        cy.get('@firstItemTitle').then(case1FirstItemTitle => {
+          expect(case1FirstItemTitle).to.equal(firstItemTitle)
+        })
+        cy.get('@lastItemTitle').then(case1LastItemTitle => {
+          expect(case1LastItemTitle).to.equal(lastItemTitle)
+        })
       })
-      expect()
-      cy.get('@lastItemTitle').then(case1LastItemTitle => {
-        expect(case1LastItemTitle).to.equal(lastItemTitle)
-      })
-    })
 
     // case 3: search with keyword Honda 1, check formData
     cy.get('.search-input').clear()
